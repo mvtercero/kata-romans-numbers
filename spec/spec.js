810f94ab56a189b2fa14481119c2a49fb01300ae
María Vedia <mvtercero85@gmail.com>
2018-05-23 11:30:49 +0200

    Refactor roman numbers with patron module

--- spec/spec.js
+++ spec/spec.js
@@ -3,10 +3,10 @@ var romanNumerals = require("../src/RomanNumerals");
 describe("Roman Numerals", function() {
     var app = romanNumerals();
     it("converts numbers to Roman Numerals", function () {
-        expect(app.romanNumerals(1)).toBe("I");
-        expect(app.romanNumerals(2)).toBe("II");
-        expect(app.romanNumerals(3)).toBe("III");
-        expect(app.romanNumerals(4)).toBe("IV");
-        expect(app.romanNumerals(2648)).toBe("MMDCXL");
+        expect(app.number(1)).toBe("I");
+        expect(app.number(2)).toBe("II");
+        expect(app.number(3)).toBe("III");
+        expect(app.number(4)).toBe("IV");
+        expect(app.number(2648)).toBe("MMDCXLVIII");
     }); 
 });
