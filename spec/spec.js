6dd87b75ae0e385fcc50a8d4ba8b2e1febd4b1e7
María Vedia <mvtercero85@gmail.com>
2018-05-23 10:55:32 +0200

    First trial to refactor code with patron module

--- spec/spec.js
+++ spec/spec.js
@@ -1,10 +1,11 @@
 describe("Roman Numerals", function() {
     it("converts numbers to Roman Numerals", function () {
         var romanNumerals = require("../src/RomanNumerals");
-        expect(romanNumerals.fromNumber(1)).toBe("I");
-        expect(romanNumerals.fromNumber(2)).toBe("II");
-        expect(romanNumerals.fromNumber(3)).toBe("III");
-        expect(romanNumerals.fromNumber(4)).toBe("IV");
-        expect(romanNumerals.fromNumber(2648)).toBe("MMDCXLVIII");
+        var app = RomanNumerals();
+        expect(romanNumerals.app.number(1)).toBe("I");
+        expect(romanNumerals.app.number(2)).toBe("II");
+        expect(romanNumerals.app.number(3)).toBe("III");
+        expect(romanNumerals.app.number(4)).toBe("IV");
+        expect(romanNumerals.app.number(2648)).toBe("MMDCXL");
     }); 
 });
--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,34 +1,38 @@
-var RomanNumerals = function () {};
+function RomanNumerals() {
+    var numeral = function(value, numeral) {
+        return {value: value, numeral: numeral};
+    };
+    
+    var numerals = [
+        numeral(1000, "M"),
+        numeral(900, "CM"),
+        numeral(500, "D"),
+        numeral(400, "CD"),
+        numeral(100, "C"),
+        numeral(90, "XC"),
+        numeral(50, "L"),
+        numeral(40, "XL"),
+        numeral(10, "X"),
+        numeral(9, "IX"),
+        numeral(5, "V"),
+        numeral(4, "IV"),
+        numeral(1, "I"),
+    ];
+    
+    function Number(number) {
+        result = "";
+        numerals.forEach(function (item) {
+            for (; number >= item.value; number -= item.value) {
+                result += item.numeral;
+            }
+        });
+    
+        return result;
+    }
 
-var numeral = function(value, numeral) {
-    return {value: value, numeral: numeral};
+    return {
+        number: Number,
+    }
+    
 };
 
-var numerals = [
-    numeral(1000, "M"),
-    numeral(900, "CM"),
-    numeral(500, "D"),
-    numeral(400, "CD"),
-    numeral(100, "C"),
-    numeral(90, "XC"),
-    numeral(50, "L"),
-    numeral(40, "XL"),
-    numeral(10, "X"),
-    numeral(9, "IX"),
-    numeral(5, "V"),
-    numeral(4, "IV"),
-    numeral(1, "I"),
-];
-
-RomanNumerals.prototype.fromNumber = function(number) {
-    result = "";
-    numerals.forEach(function (item) {
-        for (; number >= item.value; number -= item.value) {
-            result += item.numeral;
-        }
-    });
-
-    return result;
-};
-
-module.exports = new RomanNumerals();
