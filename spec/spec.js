18f5ae1ae552d40b93511cc66c0507401aa2237e
María Vedia <mvtercero85@gmail.com>
2018-05-23 09:47:27 +0200

    GREEN test converts numbers to Roman Numerals - all numbers

--- spec/spec.js
+++ spec/spec.js
@@ -5,5 +5,6 @@ describe("Roman Numerals", function() {
         expect(romanNumerals.fromNumber(2)).toBe("II");
         expect(romanNumerals.fromNumber(3)).toBe("III");
         expect(romanNumerals.fromNumber(4)).toBe("IV");
+        expect(romanNumerals.fromNumber(2648)).toBe("MMDCXLVIII");
     }); 
 });
--- src/RomanNumerals.js
+++ src/RomanNumerals.js
@@ -1,16 +1,30 @@
 var RomanNumerals = function () {};
 
+var numeral = function(value, numeral) {
+    return {value: value, numeral: numeral};
+};
+
 var numerals = [
-    {value: 4, numeral: "IV"},
-    {value: 1, numeral: "I"}
+    numeral(1000, "M"),
+    numeral(900, "CM"),
+    numeral(500, "D"),
+    numeral(400, "CD"),
+    numeral(100, "C"),
+    numeral(90, "XC"),
+    numeral(50, "L"),
+    numeral(40, "XL"),
+    numeral(10, "X"),
+    numeral(9, "IX"),
+    numeral(5, "V"),
+    numeral(4, "IV"),
+    numeral(1, "I"),
 ];
 
 RomanNumerals.prototype.fromNumber = function(number) {
     result = "";
     numerals.forEach(function (item) {
-        while (number >= item.value) {
+        for (; number >= item.value; number -= item.value) {
             result += item.numeral;
-            number -= item.value;
         }
     });
 
